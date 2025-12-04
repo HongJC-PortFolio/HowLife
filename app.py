f63e64e514e26053a64eb24ef2198ebaf50f07a6
HongJC-PortFolio <109548948+HongJC-PortFolio@users.noreply.github.com>
2025-12-04 12:15:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,32 @@ with empty1:
 
 with con1:
       st.header('1')
+      ai_insight_html = """
+<div style="
+    border: 2px solid #7e7e7e;
+    padding: 25px 30px;
+    border-radius: 12px;
+    margin-top: 20px;
+">
+    <h3 style="
+        text-align: center; 
+        margin-bottom: 12px;
+        font-weight: 700;
+    ">
+        AI의 오늘 행동 제안 ( AI Today Insight )
+    </h3>
+
+    <p style="
+        text-align: center; 
+        font-size: 17px;
+        margin-top: 5px;
+    ">
+        Ex) 물 마시지 않은 지 3시간이 지났어요. 목이 건조해지지 않게 물 한 잔 어떠세요?
+    </p>
+</div>
+"""
+
+st.markdown(ai_insight_html, unsafe_allow_html=True)
 
 with con2:
        st.header('2')
