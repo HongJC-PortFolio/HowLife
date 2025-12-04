9cafc94beb5176437784bf26106874c5ccb4987e
HongJC-PortFolio <109548948+HongJC-PortFolio@users.noreply.github.com>
2025-12-04 10:39:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ tab1,tab2,tab3,tab4=st.tabs(['aiInsight','todayGoals','aiFeedback&Cheers','weekG
 with tab1:
       st.header('AI의 오늘 행동 제안 ( AI Today Insight )')
 
-with tab1:
+with tab2:
       st.header('오늘의 목표 - 물 / 영양제 / 운동 요약')
 
-with tab1:
+with tab3:
       st.header('AI의 전 날 피드백 + 응원과 격려')
 
-with tab1:
+with tab4:
       st.header('주간 꾸준함')
