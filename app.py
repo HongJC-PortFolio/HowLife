7fb9a278f568738d78342fe7b2aef25c8a0b911e
HongJC-PortFolio <109548948+HongJC-PortFolio@users.noreply.github.com>
2025-12-04 10:36:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 import streamlit as st
 
-st.title("메인 페이지 테스트")
+st.title("HowLife - 당신의 꾸준함, AI가 함께 만듭니다.")
 
-st.write("이 화면이 보이면 app.py는 정상입니다.")
+tab1,tab2,tab3,tab4=st.tabs(['aiInsight','todayGoals','aiFeedback&Cheers','weekGraph'])
+
+with tab1:
+      st.header('AI의 오늘 행동 제안 ( AI Today Insight )')
+
+with tab1:
+      st.header('오늘의 목표 - 물 / 영양제 / 운동 요약')
+
+with tab1:
+      st.header('AI의 전 날 피드백 + 응원과 격려')
+
+with tab1:
+      st.header('주간 꾸준함')
